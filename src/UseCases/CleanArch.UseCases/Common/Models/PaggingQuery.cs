8da2af264cff24fc00c4ebdd4b2c8ea0ade4704e
Alexxey Shevchenko <shevchenko201511@gmail.com>
2023-09-15 13:18:00 +0300

    add pagging query

---
+++ src/UseCases/CleanArch.UseCases/Common/Models/PaggingQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace CleanArch.UseCases.Common.Models;
+
+public abstract record PaggingQuery<TPagingItem>(
+    int PageIndex,
+    int PageSize,
+    int SearchString,
+    string SortBy,
+    SortDirection SortDirection) : IRequest<PaggingDto<TPagingItem>>;
+
+public enum SortDirection
+{
+    Ascending,
+    Descending
+}
